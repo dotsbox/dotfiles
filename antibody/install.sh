ed9ebf50d62d3607916c25d5f5afb660510b23ba
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2016-04-09 06:51:43 +0800

    Update install.sh

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-curl -s https://raw.githubusercontent.com/getantibody/installer/master/install | bash -s
+VERSION="0.5.0"
+mkdir -p ~/.dotfiles/antibody/antibody
+wget -O /tmp/antibody_Linux_x86_64.tar.gz \
+  "https://github.com/getantibody/antibody/releases/download/$VERSION/antibody_Linux_x86_64.tar.gz"
+tar xvzf /tmp/antibody_Linux_x86_64.tar.gz -C ~/.dotfiles/antibody/antibody
